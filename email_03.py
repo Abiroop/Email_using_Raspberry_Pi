2e619e9a1e53495f87431ba7b830d88d75fa6a2b
Abiroop Mohan <94845433+Abiroop@users.noreply.github.com>
2023-10-13 13:53:36 +0530

    Update email_03.py

--- email_03.py
+++ email_03.py
@@ -4,14 +4,14 @@ from email.mime.text import MIMEText
 # Set up the email message
 msg = MIMEText('Hello, this is a test email! email_03.py')
 msg['Subject'] = 'Test Email'
-msg['From'] = 'abiroopm2@gmail.com'
+msg['From'] = 'Yourmail@gmail.com'
 msg['To'] = 'abiroopmohan@yahoo.com'
 
 # Connect to the SMTP server
 smtp_server = 'smtp.gmail.com'  # Replace with your SMTP server address
 smtp_port = 587  # Replace with your SMTP server port
-smtp_username = 'abiroopm2@gmail.com'  # Replace with your SMTP username
-smtp_password = 'mdisnyytgwpicvvq'  # Replace with your SMTP password
+smtp_username = 'Yourmail@gmail.com'  # Replace with your SMTP username
+smtp_password = 'Yourstmp_pass_not_normal_password'  # Replace with your SMTP password
 smtp_connection = smtplib.SMTP(smtp_server, smtp_port)
 smtp_connection.starttls()
 smtp_connection.login(smtp_username, smtp_password)
